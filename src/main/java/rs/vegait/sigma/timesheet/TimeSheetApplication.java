2a3aece2e620e8d0da8dd78a3360c2271cf4146a
Uros Matic <u.matic@vegait.rs>
2022-01-12 11:35:57 +0100

    initial project setup

---
+++ src/main/java/rs/vegait/sigma/timesheet/TimeSheetApplication.java
@@ -0,0 +1,13 @@
+package rs.vegait.sigma.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/vegait/sigma/timesheet/TimeSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.vegait.sigma.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
