bef9c1c2bc273cadbf760f30c4b062e1b3fe44a0
Nikola Buljcik <n.buljcik@vegait.rs>
2022-01-18 13:55:46 +0100

    #7 Adding user class and data file

---
+++ src/main/java/rs/vegaot/sigma/enumerations/UserRole.java
@@ -0,0 +1,5 @@
+package rs.vegaot.sigma.enumerations;
+
+public enum UserRole {
+	USER, ADMIN
+}
---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,6 @@
+INSERT INTO `user` (id, username, password, role)
+              VALUES (1,'uros','$$2a$10$zAnwcRbD/Vwmo/JU1GgUze5MR.bIuGYLRPSAv5IlIGWkft5E6qXaS','USER');
+INSERT INTO `user` (id, username, password, role)
+              VALUES (2,'nikola','$2a$10$zAnwcRbD/Vwmo/JU1GgUze5MR.bIuGYLRPSAv5IlIGWkft5E6qXaS','USER');
+INSERT INTO `user` (id, username, password, role)
+              VALUES (3,'luka','$2a$10$zAnwcRbD/Vwmo/JU1GgUze5MR.bIuGYLRPSAv5IlIGWkft5E6qXaS','ADMIN');
